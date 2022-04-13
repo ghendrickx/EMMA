42cf77c62cc444823434ec5d5f18093aa531f270
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2022-04-13 09:28:43 +0200

    MOD: ECO.py > _ecotopes_overview.py: clearer naming that is less ambigious (functionality remains the same)

---
+++ model/_ecotopes_overview.py
@@ -0,0 +1,6 @@
+ECOTOPES = {
+        ('brackish', 'sub-littoral', 'low energy', 'deep', 'rich in silt') : 'B2_122s',
+        ('brackish', 'sub-littoral', 'low energy', 'deep', 'fine sands') : 'B2_122f',
+}
+
+# print(ECOTOPES)
