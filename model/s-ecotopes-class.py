eb7afdbe0b337fc6589422950ddd311c68ab2b27
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2022-04-13 09:49:49 +0200

    ADD: s-ecotopes-class.py > Cell: added ecotope-related properties to look-up the ecotope based on the provided abiotic characteristics of the cell.

--- model/s-ecotopes-class.py
+++ model/s-ecotopes-class.py
@@ -1,4 +1,11 @@
+from model._ecotopes_overview import ECOTOPES
+
+
 class Cell:
+    # As the grid also uses Cell-objects, you may want to rename this object;
+    # e.g. to Ecotope, or CellEcotope, or CellVariables, or ...?
+    # Then, this object can be coupled to the Cell-object in grid.py.
+
     def __init__(self, salinity, depth1, hydrodynamics, depth2, substratum2):
         self.salinity = salinity
         self.depth1 = depth1
@@ -7,6 +14,37 @@ class Cell:
         self.substratum2 = substratum2
 
         self.salinity_label = None
+        self.depth1_label = None
+        self.hydrodynamics_label = None
+        self.depth2_label = None
+        self.substratum2_label = None
+
+        self._ecotope = None
+
+    @property
+    def ecotope_labels(self):
+        """The collection of labels used to look-up the ecotope in the ECOTOPES-overview.
+
+        :return: collection of ecotope-labels
+        :rtype: tuple(str)
+        """
+        return (
+            self.salinity_label, self.depth1_label, self.hydrodynamics_label, self.depth2_label, self.substratum2_label
+        )
+
+    @property
+    def ecotope(self):
+        """The ecotope of the cell based on the cell's abiotic characteristics.
+
+        :return: ecotope
+        :rtype: str
+        """
+        if self._ecotope is None:
+            # this additional if-statement is included so you don't have to re-determine the ecotope everytime you
+            # access it, only the first time, when it is still undefined.
+            self._ecotope = ECOTOPES.get(self.ecotope_labels, 'undefined')
+
+        return self._ecotope
 
 
 # def label_salinity(salinity):
