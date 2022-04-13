3867304061f89e9966518bb62732cf0ef6517983
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2022-04-13 09:29:34 +0200

    DOC: _ecotopes_overview.py: added documentation to the python-file.

--- model/_ecotopes_overview.py
+++ model/_ecotopes_overview.py
@@ -1,3 +1,7 @@
+"""
+Overview of the ecotopes included in the model.
+"""
+
 ECOTOPES = {
         ('brackish', 'sub-littoral', 'low energy', 'deep', 'rich in silt') : 'B2_122s',
         ('brackish', 'sub-littoral', 'low energy', 'deep', 'fine sands') : 'B2_122f',
