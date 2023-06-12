b5e1c6f056cce45bd0da651427938692c28ee6e9
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-06-12 17:27:13 +0200

    ADD: tests/ > test_config.py: added file for testing the configuration setting(s).

---
+++ tests/test_config.py
@@ -0,0 +1,12 @@
+"""
+Tests for `config_file.py`.
+
+Author: Gijs G. Hendrickx
+"""
+from config.config_file import load_config
+
+"""TestClasses"""
+
+
+class TestLoadConfig:
+    """Tests for `load_config()`."""
