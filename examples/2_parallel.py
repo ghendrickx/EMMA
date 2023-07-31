c9d91fd8e80af22827f4e0d116d51f2edf586f8b
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-07-31 11:58:56 +0200

    ADD: examples/ > 2_parallel.py: added example of parallel-usage.

---
+++ examples/2_parallel.py
@@ -0,0 +1,16 @@
+"""
+Example 2: Analysing a multiple files with hydrodynamic output data in parallel.
+
+Authors: Soesja Brunink & Gijs G. Hendrickx
+"""
+from examples.example_plot import create_figure
+from src.processing import map_ecotopes
+
+# map ecotopes based on hydrodynamic output data: 'file-name-0_map.nc', 'file-name-1_map.nc'
+results = map_ecotopes(
+    'file-name-0_map.nc', 'file-name-1_map.nc',
+    n_cores=2
+)
+
+# plot spatial distribution of ecotopes, i.e., an ecotope-map
+create_figure(results)
