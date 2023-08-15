b39102817ca563256af3fb2ade3c341272ec3ae9
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-08-15 14:03:02 +0200

    DEBUG: 2_parallel.py: placed the function-calls in the `main`-environment for valid parallel computations.

--- examples/2_parallel.py
+++ examples/2_parallel.py
@@ -6,12 +6,15 @@ Authors: Soesja Brunink & Gijs G. Hendrickx
 from examples._example_plot import create_figure
 from src.processing import map_ecotopes
 
-# map ecotopes based on hydrodynamic output data: 'file-name-0_map.nc', 'file-name-1_map.nc'
-results = map_ecotopes(
-    'file-name-0_map.nc', 'file-name-1_map.nc',
-    wd='directory/to/output/files',
-    n_cores=2
-)
+# TODO: for parallel computations, the `map_ecotopes()`-function must be called within the `if __name__ == '__main__'`-
+#  statement for the `multiprocessing`-package to work properly.
+if __name__ == '__main__':
+    # map ecotopes based on hydrodynamic output data: 'file-name-0_map.nc', 'file-name-1_map.nc'
+    results = map_ecotopes(
+        'file-name-0_map.nc', 'file-name-1_map.nc',
+        wd='directory/to/output/files',
+        n_cores=2
+    )
 
-# plot spatial distribution of ecotopes, i.e., an ecotope-map
-create_figure(results)
+    # plot spatial distribution of ecotopes, i.e., an ecotope-map
+    create_figure(results)
