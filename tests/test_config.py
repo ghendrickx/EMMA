0ea21dd0e94529247294fa313d2084019e6b4bb8
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-08-17 11:14:03 +0200

    REF: test_config.py: set configuration outside test-class (as suggested in `e35e3f184593ee28564fb4c8d839c864f1b10b5d`).

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,71 +5,68 @@ Author: Gijs G. Hendrickx
 """
 from config.config_file import load_config
 
+"""Set configuration"""
+TEST_CONFIG = load_config('emma.json')
+
 """TestClasses"""
 
 
-# TODO: Add a file-level variable (e.g., `TEST_CONFIG`) in which to load the configuration once, and use it in all
-#  subsequent tests.
 class TestLoadConfig:
     """Tests for `load_config()`."""
 
-    def setup_method(self):
-        """Initialise with default configuration."""
-        self.config = load_config('emma.json')
-
     def test_keys(self):
         """Test if all required key-words are in the configuration file: depth 1."""
-        keys = list(self.config.keys())
+        keys = list(TEST_CONFIG.keys())
         for k in ('salinity', 'depth-1', 'hydrodynamics', 'depth-2', 'substratum-2'):
             assert k in keys
 
     def test_keys_salinity(self):
         """Test if all required key-words for the 'salinity'-key are in the configuration file: depth 2."""
-        keys = list(self.config['salinity'].keys())
+        keys = list(TEST_CONFIG['salinity'].keys())
         for k in ('variable', 'fresh', 'marine'):
             assert k in keys
 
     def test_keys_depth_1(self):
         """Test if all required key-words for the 'depth 1'-key are in the configuration file: depth 2."""
-        keys = list(self.config['depth-1'].keys())
+        keys = list(TEST_CONFIG['depth-1'].keys())
         for k in ('low-water', 'high-water'):
             assert k in keys
 
     def test_keys_hydrodynamics(self):
         """Test if all required key-words for the 'hydrodynamics'-key are in the configuration file: depth 2."""
-        keys = list(self.config['hydrodynamics'].keys())
+        keys = list(TEST_CONFIG['hydrodynamics'].keys())
         for k in ('stagnant', 'sub-littoral', 'littoral'):
             assert k in keys
 
     def test_keys_depth_2(self):
         """Test if all required key-words for the 'depth 2'-key are in the configuration file: depth 2."""
-        keys = list(self.config['depth-2'].keys())
+        keys = list(TEST_CONFIG['depth-2'].keys())
         for k in ('sub-littoral', 'littoral', 'supra-littoral'):
             assert k in keys
 
     def test_keys_substratum_2(self):
         """Test if all required key-words for the 'substratum 2'-key are in the configuration file: depth 2."""
-        keys = list(self.config['substratum-2'].keys())
+        keys = list(TEST_CONFIG['substratum-2'].keys())
         for k in ('soft',):
             assert k in keys
 
     def test_keys_depth_2_sub_littoral(self):
         """Test if all required key-words for the 'sub-littoral'-key in the 'depth 2'-key are in the configuration file:
         depth 3."""
-        keys = list(self.config['depth-2']['sub-littoral'].keys())
+        keys = list(TEST_CONFIG['depth-2']['sub-littoral'].keys())
         for k in ('depth-deep', 'depth-shallow', 'low-water'):
             assert k in keys
 
     def test_keys_depth_2_littoral(self):
         """Test if all required key-words for the 'littoral'-key in the 'depth 2'-key are in the configuration file:
         depth 3."""
-        keys = list(self.config['depth-2']['littoral'].keys())
+        keys = list(TEST_CONFIG['depth-2']['littoral'].keys())
         for k in ('inundation-upper', 'inundation-lower'):
             assert k in keys
 
     def test_keys_depth_2_supra_littoral(self):
         """Test if all required key-words for the 'supra-littoral'-key in the 'depth 2'-key are in the configuration
         file: depth 3."""
-        keys = list(self.config['depth-2']['supra-littoral'].keys())
+        keys = list(TEST_CONFIG['depth-2']['supra-littoral'].keys())
         for k in ('frequency-1', 'frequency-2', 'frequency-3'):
             assert k in keys
