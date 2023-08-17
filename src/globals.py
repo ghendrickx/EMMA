c0d901dc6d9bfa772333d718afc1106f752239e6
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-08-17 09:13:41 +0200

    ADD: globals.py: added global variable (`CONFIG`) and complex-typesettings.

---
+++ src/globals.py
@@ -0,0 +1,11 @@
+"""
+Collection of global variables.
+
+Authors: Soesja Brunink & Gijs G. Hendrickx
+"""
+import typing
+
+CONFIG = dict()
+
+_TypeXYLabel = typing.Dict[typing.Tuple[float, float], str]
+_TypeXYBool = typing.Dict[typing.Tuple[float, float], bool]
