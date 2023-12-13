c4b616606798848994e21928546a1a7c9ff2e428
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-13 18:38:44 +0100

    REF: test_labelling.py > TestSubstratum2Code: replaced TestClass with parametrize-pytest function.

--- tests/test_labelling.py
+++ tests/test_labelling.py
@@ -95,78 +95,27 @@ def test_hydrodynamics_label(velocity, depth1, label):
     ]
 )
 def test_depth2_label(sub1, depth1, depth, inundated, frequency, label):
+    """Test labelling of Depth 2."""
     out = lab.depth_2_code(str(sub1), str(depth1), depth, inundated, frequency)
     assert out.lower() == str(label)
 
 
-class TestSubstratum2Code:
-    """Tests for `substratum_2_code()`."""
-
-    def test_undefined(self):
-        """Test the ecotope-code determination when insufficient data is available."""
-        output = lab.substratum_2_code('x', '', 0.)
-        assert output == 'x'
-
-    def test_unknown(self):
-        """Test the ecotope-code determination when insufficient data is available."""
-        # noinspection PyTypeChecker
-        output = lab.substratum_2_code(None, '', 0.)
-        assert output == 'x'
-
-    def test_hydrodynamics_undefined(self):
-        """Test the ecotope-code determination when insufficient data is available."""
-        output = lab.substratum_2_code('1', 'x', 0.)
-        assert output == 'x'
-
-    def test_hydrodynamics_unknown(self):
-        """Test the ecotope-code determination when insufficient data is available."""
-        # noinspection PyTypeChecker
-        output = lab.substratum_2_code('1', None, 0.)
-        assert output == 'x'
-
-    def test_hard_low_stone_wood(self):
-        """Test the ecotope-code determination resulting in 'stond/wood' ('2') for 'hard substratum' ('1') and 'low
-        energy' ('2') conditions.
-        """
-        output = lab.substratum_2_code('1', '2', 0.)
-        assert output == '1'
-
-    def test_hard_stagnant_stone_wood(self):
-        """Test the ecotope-code determination resulting in 'stond/wood' ('2') for 'hard substratum' ('1') and
-        'stagnant' ('3') conditions.
-        """
-        output = lab.substratum_2_code('1', '3', 0.)
-        assert output == '1'
-
-    def test_hard_peat(self):
-        """Test the ecotope-code determination resulting in 'peat' ('2') for 'hard substratum' ('1') and 'high energy'
-        ('2') conditions.
-        """
-        output = lab.substratum_2_code('1', '1', 0.)
-        assert output == '2'
-
-    def test_grains_unknown(self):
-        """Test the ecotope-code determination when insufficient data is available."""
-        # noinspection PyTypeChecker
-        output = lab.substratum_2_code('2', '', None)
-        assert output == 'x'
-
-    def test_soft_silt(self):
-        """Test the ecotope-code determination resulting in 'silt' ('s') for 'soft substratum' ('2')."""
-        output = lab.substratum_2_code('2', '', 20)
-        assert output == 's'
-
-    def test_soft_fines(self):
-        """Test the ecotope-code determination resulting in 'fine sand' ('f') for 'soft substratum' ('2')."""
-        output = lab.substratum_2_code('2', '', 100)
-        assert output == 'f'
-
-    def test_soft_sand(self):
-        """Test the ecotope-code determination resulting in 'coarse sand' ('z') for 'soft substratum' ('2')."""
-        output = lab.substratum_2_code('2', '', 2000)
-        assert output == 'z'
-
-    def test_soft_gravel(self):
-        """Test the ecotope-code determination resulting in 'gravel' ('g') for 'soft substratum' ('2')."""
-        output = lab.substratum_2_code('2', '', 3000)
-        assert output == 'g'
+@pytest.mark.parametrize(
+    'sub1, hydro, grain_size, label',
+    [
+        ('x', '', 0, 'x'),
+        (1, 'x', 0, 'x'),
+        (1, 2, 0, 1),
+        (1, 3, 0, 1),
+        (1, 1, 0, 2),
+        (2, '', None, 'x'),
+        (2, '', 20, 's'),
+        (2, '', 100, 'f'),
+        (2, '', 2000, 'z'),
+        (2, '', 3000, 'g'),
+    ]
+)
+def test_substratum2_label(sub1, hydro, grain_size, label):
+    """Test labelling of Substratum 2."""
+    out = lab.substratum_2_code(str(sub1), str(hydro), grain_size)
+    assert out.lower() == str(label)
