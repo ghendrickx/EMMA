ddeeb810de81b3e00d16a5e138eb76664cadf329
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-13 18:07:33 +0100

    Added tests for export._default_file_name()

--- tests/test_export.py
+++ tests/test_export.py
@@ -25,3 +25,19 @@ from src import export as exp
 def test_file_dir(file_name, wd, expected):
     file = exp.file_dir(file_name, wd)
     assert file == expected
+
+
+@pytest.mark.parametrize(
+    'file_name, default, extension, expected',
+    [
+        ('file.txt', 'f.txt', None, 'file.txt'),
+        ('file', 'f.txt', None, 'file.txt'),
+        ('file', 'f.txt', '.csv', 'file.csv'),
+        ('file.csv', 'f.txt', '.txt', 'file.txt'),
+        ('file', 'f.txt', 'txt', 'file.txt'),
+        ('file.txt', 'f_net.txt', '_net.txt', 'file_net.txt'),
+    ]
+)
+def test_default_file_name(file_name, default, extension, expected):
+    file = exp._default_file_name(file_name, default, extension)
+    assert file == expected
