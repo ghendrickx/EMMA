b9a162d45502daf20a717456cb7a00cfbbac89fe
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-14 17:11:40 +0100

    ADD: test_processing.py > test_convert2tuple(): Added tests for convert2tuple().

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -3,6 +3,7 @@ Tests for `src/processing.py`.
 
 Author: Gijs G. Hendrickx
 """
+import numpy.testing as npt
 import pytest
 
 from src import processing
@@ -21,12 +22,32 @@ from src import processing
         ([0, 0], [0, 0], ['c', 'c'], {
             (0, 0): 'c',
         }),
-        ([0, 0], [0, 1], ['c', 'z'], {
-            (0, 0): 'c',
-            (0, 1): 'z',
+        ([0, 0], [0, 1], ['c1', 'c2'], {
+            (0, 0): 'c1',
+            (0, 1): 'c2',
         })
     ]
 )
 def test_convert2dict(x, y, labels, expected):
     out = processing.convert2dict(x, y, labels)
     assert out == expected
+
+
+@pytest.mark.parametrize(
+    'xy_labels, expected',
+    [
+        ({(0, 0): 'c'}, ([0], [0], ['c'])),
+        ({
+            (0, 0): 'c',
+            (0, 1): 'c',
+        }, ([0, 0], [0, 1], ['c', 'c'])),
+        ({
+            (0, 0): 'c1',
+            (0, 1): 'c2',
+        }, ([0, 0], [0, 1], ['c1', 'c2'])),
+    ]
+)
+def test_convert2tuple(xy_labels, expected):
+    out = processing.convert2tuple(xy_labels)
+    for x, y in zip(out, expected):
+        npt.assert_array_equal(x, y)
