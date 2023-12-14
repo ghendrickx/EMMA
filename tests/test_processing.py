b86c62ff07de86e1094c28db8b704d2ceec1f5e1
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-14 17:02:30 +0100

    DEL: test_processing.py > equal_dict(): Removed redundant dict-equality tester.

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -8,29 +8,25 @@ import pytest
 from src import processing
 
 
-def equal_dict(test: dict, expected: dict):
-    assert all(k in expected for k in test)
-    assert all(k in test for k in expected)
-    assert all(expected[k] == v for k, v in test.items())
-    assert all(test[k] == v for k, v in expected.items())
-
-
 @pytest.mark.parametrize(
     'x, y, labels, expected',
     [
+        ([0], [0], ['c'], {
+            (0, 0): 'c',
+        }),
         ([0, 0], [0, 1], ['c', 'c'], {
             (0, 0): 'c',
-            (0, 1): 'c'
+            (0, 1): 'c',
         }),
         ([0, 0], [0, 0], ['c', 'c'], {
             (0, 0): 'c',
         }),
         ([0, 0], [0, 1], ['c', 'z'], {
             (0, 0): 'c',
-            (0, 1): 'z'
+            (0, 1): 'z',
         })
     ]
 )
 def test_convert2dict(x, y, labels, expected):
     out = processing.convert2dict(x, y, labels)
-    equal_dict(out, expected)
+    assert out == expected
