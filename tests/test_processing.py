8d206811d212070140ef9b062d515ccc0cadc7d4
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-15 10:04:12 +0100

    ADD: test_processing.py > test_determine_ecotopes(): Added test for ecotopes determining function.

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -3,6 +3,8 @@ Tests for `src/processing.py`.
 
 Author: Gijs G. Hendrickx
 """
+import os
+
 import numpy.testing as npt
 import pytest
 
@@ -50,4 +52,19 @@ def test_convert2dict(data, expected):
 def test_convert2tuple(data, expected):
     out = processing.convert2tuple(data)
     for x, y in zip(out, expected):
+        npt.assert_array_equal(x, y)
+
+
+def test_determine_ecotopes():
+    wd = __file__.split(os.sep)[:-2] + ['examples', 'ex_map_data']
+    nc_file = os.sep.join(wd + ['output_map.nc'])
+    csv_file = os.sep.join(wd + ['output.csv'])
+
+    test = processing.__determine_ecotopes(nc_file, f_map_config='dfm1.json')
+
+    with open(csv_file, mode='r') as f:
+        file = [line.rstrip().split(',') for line in f.readlines()]
+    expected = tuple(zip(*file))
+
+    for x, y in zip(test, expected):
         npt.assert_array_equal(x, y)
