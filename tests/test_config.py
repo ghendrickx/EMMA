e0c5f86f6c5db5c0b2b6c22da5839228c217871c
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-15 11:38:10 +0100

    REF: test_config.py: converted TestClass to pytest.mark.parametrize-implementation.

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,70 +3,27 @@ Tests for `config/config_file.py`.
 
 Author: Gijs G. Hendrickx
 """
+import pytest
+
 from config.config_file import load_config
 
-"""Set configuration"""
 TEST_CONFIG = load_config('emma.json')
 
-"""TestClasses"""
 
-
-class TestLoadConfig:
-    """Tests for `load_config()`."""
-
-    def test_keys(self):
-        """Test if all required key-words are in the configuration file: depth 1."""
-        keys = list(TEST_CONFIG.keys())
-        for k in ('salinity', 'depth-1', 'hydrodynamics', 'depth-2', 'substratum-2'):
-            assert k in keys
-
-    def test_keys_salinity(self):
-        """Test if all required key-words for the 'salinity'-key are in the configuration file: depth 2."""
-        keys = list(TEST_CONFIG['salinity'].keys())
-        for k in ('variable', 'fresh', 'marine'):
-            assert k in keys
-
-    def test_keys_depth_1(self):
-        """Test if all required key-words for the 'depth 1'-key are in the configuration file: depth 2."""
-        keys = list(TEST_CONFIG['depth-1'].keys())
-        for k in ('low-water', 'high-water'):
-            assert k in keys
-
-    def test_keys_hydrodynamics(self):
-        """Test if all required key-words for the 'hydrodynamics'-key are in the configuration file: depth 2."""
-        keys = list(TEST_CONFIG['hydrodynamics'].keys())
-        for k in ('stagnant', 'sub-littoral', 'littoral'):
-            assert k in keys
-
-    def test_keys_depth_2(self):
-        """Test if all required key-words for the 'depth 2'-key are in the configuration file: depth 2."""
-        keys = list(TEST_CONFIG['depth-2'].keys())
-        for k in ('sub-littoral', 'littoral', 'supra-littoral'):
-            assert k in keys
-
-    def test_keys_substratum_2(self):
-        """Test if all required key-words for the 'substratum 2'-key are in the configuration file: depth 2."""
-        keys = list(TEST_CONFIG['substratum-2'].keys())
-        for k in ('soft',):
-            assert k in keys
-
-    def test_keys_depth_2_sub_littoral(self):
-        """Test if all required key-words for the 'sub-littoral'-key in the 'depth 2'-key are in the configuration file:
-        depth 3."""
-        keys = list(TEST_CONFIG['depth-2']['sub-littoral'].keys())
-        for k in ('depth-deep', 'depth-shallow'):
-            assert k in keys
-
-    def test_keys_depth_2_littoral(self):
-        """Test if all required key-words for the 'littoral'-key in the 'depth 2'-key are in the configuration file:
-        depth 3."""
-        keys = list(TEST_CONFIG['depth-2']['littoral'].keys())
-        for k in ('inundation-upper', 'inundation-lower'):
-            assert k in keys
-
-    def test_keys_depth_2_supra_littoral(self):
-        """Test if all required key-words for the 'supra-littoral'-key in the 'depth 2'-key are in the configuration
-        file: depth 3."""
-        keys = list(TEST_CONFIG['depth-2']['supra-littoral'].keys())
-        for k in ('frequency-1', 'frequency-2', 'frequency-3'):
-            assert k in keys
+@pytest.mark.parametrize(
+    'dictionary, expected',
+    [
+        (TEST_CONFIG, ('salinity', 'depth-1', 'hydrodynamics', 'depth-2', 'substratum-2')),
+        (TEST_CONFIG['salinity'], ('variable', 'fresh', 'marine')),
+        (TEST_CONFIG['depth-1'], ('low-water', 'high-water')),
+        (TEST_CONFIG['hydrodynamics'], ('stagnant', 'sub-littoral', 'littoral')),
+        (TEST_CONFIG['depth-2'], ('sub-littoral', 'littoral', 'supra-littoral')),
+        (TEST_CONFIG['substratum-2'], ('soft',)),
+        (TEST_CONFIG['depth-2']['sub-littoral'], ('depth-deep', 'depth-shallow')),
+        (TEST_CONFIG['depth-2']['littoral'], ('inundation-upper', 'inundation-lower')),
+        (TEST_CONFIG['depth-2']['supra-littoral'], ('frequency-1', 'frequency-2', 'frequency-3')),
+    ]
+)
+def test_keys(dictionary, expected):
+    keys = list(dictionary.keys())
+    assert all(k in keys for k in expected)
