06cd9e6065b2a34c2ca87c28a388d362c8bbf090
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-15 09:30:00 +0100

    REF: test_processing.py: refactored use of pytest.mark.parametrize()-decorator.

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -10,31 +10,31 @@ from src import processing
 
 
 @pytest.mark.parametrize(
-    'x, y, labels, expected',
+    'data, expected',
     [
-        ([0], [0], ['c'], {
+        (([0], [0], ['c']), {
             (0, 0): 'c',
         }),
-        ([0, 0], [0, 1], ['c', 'c'], {
+        (([0, 0], [0, 1], ['c', 'c']), {
             (0, 0): 'c',
             (0, 1): 'c',
         }),
-        ([0, 0], [0, 0], ['c', 'c'], {
+        (([0, 0], [0, 0], ['c', 'c']), {
             (0, 0): 'c',
         }),
-        ([0, 0], [0, 1], ['c1', 'c2'], {
+        (([0, 0], [0, 1], ['c1', 'c2']), {
             (0, 0): 'c1',
             (0, 1): 'c2',
         })
     ]
 )
-def test_convert2dict(x, y, labels, expected):
-    out = processing.convert2dict(x, y, labels)
+def test_convert2dict(data, expected):
+    out = processing.convert2dict(*data)
     assert out == expected
 
 
 @pytest.mark.parametrize(
-    'xy_labels, expected',
+    'data, expected',
     [
         ({(0, 0): 'c'}, ([0], [0], ['c'])),
         ({
@@ -47,7 +47,7 @@ def test_convert2dict(x, y, labels, expected):
         }, ([0, 0], [0, 1], ['c1', 'c2'])),
     ]
 )
-def test_convert2tuple(xy_labels, expected):
-    out = processing.convert2tuple(xy_labels)
+def test_convert2tuple(data, expected):
+    out = processing.convert2tuple(data)
     for x, y in zip(out, expected):
         npt.assert_array_equal(x, y)
