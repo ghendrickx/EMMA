b8a0a6825d2d2ea8faa29dccab5dcd12880fa839
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-15 11:30:14 +0100

    REF: test_performance.py: refactored all tests to use the pytest.mark.parametrize-decorator.

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -7,48 +7,33 @@ import pytest
 
 from src import performance as pf
 
-"""pytest.fixtures"""
 
-
-@pytest.fixture
-def xy_labels_1():
-    """Ecotope-data with three types of ecotopes without the use of wild cards (i.e., 'x')."""
-    return {
-        (0, 0): 'Z2.222f',
-        (0, 1): 'Z2.222f',
-        (1, 0): 'Z2.222s',
-        (1, 1): 'Z2.221s',
-    }
-
-
-@pytest.fixture
-def xy_labels_2():
-    """Ecotope-data with two types of ecotopes without the use of wild cards (i.e., 'x')."""
-    return {
-        (0, 0): 'Z2.222f',
-        (0, 1): 'Z2.222f',
-        (1, 0): 'Z2.221s',
-        (1, 1): 'Z2.221s',
-    }
-
-
-@pytest.fixture
-def xy_labels_3():
-    """Ecotope-data with three types of ecotopes with the use of wild cards (i.e., 'x')."""
-    return {
-        (0, 0): 'Z2.222f',
-        (0, 1): 'Z2.222f',
-        (1, 0): 'Z2.22xs',
-        (1, 1): 'Z2.221s',
-    }
+__XY_LABELS_1 = {
+    (0, 0): 'Z2.222f',
+    (0, 1): 'Z2.222f',
+    (1, 0): 'Z2.222s',
+    (1, 1): 'Z2.221s',
+}
+__XY_LABELS_2 = {
+    (0, 0): 'Z2.222f',
+    (0, 1): 'Z2.222f',
+    (1, 0): 'Z2.221s',
+    (1, 1): 'Z2.221s',
+}
+__XY_LABELS_3 = {
+    (0, 0): 'Z2.222f',
+    (0, 1): 'Z2.222f',
+    (1, 0): 'Z2.22xs',
+    (1, 1): 'Z2.221s',
+}
 
 
 """helper functions"""
 
 
-def comparison_exec(*data, level=None):
+def comparison_exec(*data, wild_card=True, level=None, label=False):
     comp = pf.Comparison(*data)
-    return comp.exec(level)
+    return comp.exec(level, enable_wild_card=wild_card, specific_label=label)
 
 
 def n_correct(perf):
@@ -63,138 +48,75 @@ def tuple_correct(perf):
     return n_correct(perf), n_incorrect(perf)
 
 
-"""TestClasses"""
+@pytest.mark.parametrize(
+    'out1, out2, correct',
+    [
+        (__XY_LABELS_1, __XY_LABELS_1, (4, 0)),
+        (__XY_LABELS_2, __XY_LABELS_2, (4, 0)),
+        (__XY_LABELS_3, __XY_LABELS_3, (4, 0)),
+        (__XY_LABELS_1, __XY_LABELS_2, (3, 1)),
+        (__XY_LABELS_1, __XY_LABELS_3, (3, 1)),
+        (__XY_LABELS_2, __XY_LABELS_3, (3, 1)),
+    ]
+)
+def test_matches(out1, out2, correct):
+    out = comparison_exec(out1, out2)
+    assert tuple_correct(out) == correct
 
 
-class TestPerformance:
-    """Tests for `Comparison`."""
+@pytest.mark.parametrize(
+    'out1, out2, wild_card, correct',
+    [
+        (__XY_LABELS_3, __XY_LABELS_1, True, (4, 0)),
+        (__XY_LABELS_3, __XY_LABELS_2, True, (4, 0)),
+        (__XY_LABELS_3, __XY_LABELS_1, False, (3, 1)),
+        (__XY_LABELS_3, __XY_LABELS_2, False, (3, 1)),
+    ]
+)
+def test_wild_card(out1, out2, wild_card, correct):
+    out = comparison_exec(out1, out2, wild_card=wild_card)
+    assert tuple_correct(out) == correct
 
-    """perfect matches"""
 
-    def test_full_perfect_match_1(self, xy_labels_1):
-        out = comparison_exec(xy_labels_1, xy_labels_1)
-        assert tuple_correct(out) == (4, 0)
+@pytest.mark.parametrize(
+    'out1, out2, level, correct',
+    [
+        (__XY_LABELS_1, __XY_LABELS_2, 6, (3, 1)),
+        (__XY_LABELS_1, __XY_LABELS_2, 5, (3, 1)),
+        (__XY_LABELS_1, __XY_LABELS_2, 4, (4, 0)),
+        (__XY_LABELS_1, __XY_LABELS_2, 3, (4, 0)),
+        (__XY_LABELS_1, __XY_LABELS_2, 2, (4, 0)),
+        (__XY_LABELS_1, __XY_LABELS_2, 1, (4, 0)),
+    ]
+)
+def test_max_level(out1, out2, level, correct):
+    out = comparison_exec(out1, out2, level=level)
+    assert tuple_correct(out) == correct
 
-    def test_full_perfect_match_2(self, xy_labels_2):
-        out = comparison_exec(xy_labels_2, xy_labels_2)
-        assert tuple_correct(out) == (4, 0)
 
-    def test_full_perfect_match_3(self, xy_labels_3):
-        out = comparison_exec(xy_labels_3, xy_labels_3)
-        assert tuple_correct(out) == (4, 0)
+@pytest.mark.parametrize(
+    'out1, out2, level, correct',
+    [
+        (__XY_LABELS_1, __XY_LABELS_2, 0, (4, 0)),
+        (__XY_LABELS_1, __XY_LABELS_2, 1, (4, 0)),
+        (__XY_LABELS_1, __XY_LABELS_2, 2, (4, 0)),
+        (__XY_LABELS_1, __XY_LABELS_2, 3, (4, 0)),
+        (__XY_LABELS_1, __XY_LABELS_2, 4, (3, 1)),
+        (__XY_LABELS_1, __XY_LABELS_2, 5, (4, 0)),
+    ]
+)
+def test_specific_label(out1, out2, level, correct):
+    out = comparison_exec(out1, out2, level=level, label=True)
+    assert tuple_correct(out) == correct
 
-    """imperfect matches"""
 
-    def test_full_match_1_2(self, xy_labels_1, xy_labels_2):
-        out = comparison_exec(xy_labels_1, xy_labels_2)
-        assert tuple_correct(out) == (3, 1)
-
-    def test_full_match_1_3(self, xy_labels_1, xy_labels_3):
-        out = comparison_exec(xy_labels_1, xy_labels_3)
-        assert tuple_correct(out) == (3, 1)
-
-    def test_full_match_2_3(self, xy_labels_2, xy_labels_3):
-        out = comparison_exec(xy_labels_2, xy_labels_3)
-        assert tuple_correct(out) == (3, 1)
-
-    """wild card usage"""
-
-    def test_full_match_w_wild_card_1(self, xy_labels_3, xy_labels_1):
-        out = comparison_exec(xy_labels_3, xy_labels_1)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_full_match_w_wild_card_2(self, xy_labels_3, xy_labels_2):
-        out = comparison_exec(xy_labels_3, xy_labels_2)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_full_match_wo_wild_card_1(self, xy_labels_3, xy_labels_1):
-        comp = pf.Comparison(xy_labels_3, xy_labels_1)
-        out = comp.exec(None, enable_wild_card=False)
-        assert tuple_correct(out) == (3, 1)
-
-    def test_full_match_wo_wild_card_2(self, xy_labels_3, xy_labels_2):
-        comp = pf.Comparison(xy_labels_3, xy_labels_2)
-        out = comp.exec(None, enable_wild_card=False)
-        assert tuple_correct(out) == (3, 1)
-
-    def test_full_match_w_diff_wild_card_1(self, xy_labels_3, xy_labels_1):
-        comp = pf.Comparison(xy_labels_3, xy_labels_1, wild_card='o')
-        out = comp.exec(None, enable_wild_card=True)
-        assert tuple_correct(out) == (3, 1)
-
-    def test_full_match_w_diff_wild_card_2(self, xy_labels_3, xy_labels_2):
-        comp = pf.Comparison(xy_labels_3, xy_labels_2, wild_card='o')
-        out = comp.exec(None, enable_wild_card=True)
-        assert tuple_correct(out) == (3, 1)
-
-    """performance with max. level"""
-
-    def test_level_6_match_1_2(self, xy_labels_1, xy_labels_2):
-        out = comparison_exec(xy_labels_1, xy_labels_2, level=6)
-        assert tuple_correct(out) == (3, 1)
-
-    def test_level_5_match_1_2(self, xy_labels_1, xy_labels_2):
-        out = comparison_exec(xy_labels_1, xy_labels_2, level=5)
-        assert tuple_correct(out) == (3, 1)
-
-    def test_level_4_match_1_2(self, xy_labels_1, xy_labels_2):
-        out = comparison_exec(xy_labels_1, xy_labels_2, level=4)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_level_3_match_1_2(self, xy_labels_1, xy_labels_2):
-        out = comparison_exec(xy_labels_1, xy_labels_2, level=3)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_level_2_match_1_2(self, xy_labels_1, xy_labels_2):
-        out = comparison_exec(xy_labels_1, xy_labels_2, level=2)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_level_1_match_1_2(self, xy_labels_1, xy_labels_2):
-        out = comparison_exec(xy_labels_1, xy_labels_2, level=1)
-        assert tuple_correct(out) == (4, 0)
-
-    """performance specific label"""
-
-    def test_label_6_match_1_2(self, xy_labels_1, xy_labels_2):
-        comp = pf.Comparison(xy_labels_1, xy_labels_2)
-        out = comp.exec(5, specific_label=True)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_label_5_match_1_2(self, xy_labels_1, xy_labels_2):
-        comp = pf.Comparison(xy_labels_1, xy_labels_2)
-        out = comp.exec(4, specific_label=True)
-        assert tuple_correct(out) == (3, 1)
-
-    def test_label_4_match_1_2(self, xy_labels_1, xy_labels_2):
-        comp = pf.Comparison(xy_labels_1, xy_labels_2)
-        out = comp.exec(3, specific_label=True)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_label_3_match_1_2(self, xy_labels_1, xy_labels_2):
-        comp = pf.Comparison(xy_labels_1, xy_labels_2)
-        out = comp.exec(2, specific_label=True)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_label_2_match_1_2(self, xy_labels_1, xy_labels_2):
-        comp = pf.Comparison(xy_labels_1, xy_labels_2)
-        out = comp.exec(1, specific_label=True)
-        assert tuple_correct(out) == (4, 0)
-
-    def test_label_1_match_1_2(self, xy_labels_1, xy_labels_2):
-        comp = pf.Comparison(xy_labels_1, xy_labels_2)
-        out = comp.exec(0, specific_label=True)
-        assert tuple_correct(out) == (4, 0)
-
-    """error-testing"""
-
-    def test_error_negative_level(self, xy_labels_1):
-        comp = pf.Comparison(xy_labels_1, xy_labels_1)
-        with pytest.raises(ValueError):
-            comp.exec(-1)
-        comp.exec(10)
-
-    def test_error_invalid_index(self, xy_labels_1):
-        comp = pf.Comparison(xy_labels_1, xy_labels_1)
-        with pytest.raises(ValueError):
-            comp.exec(6, specific_label=True)
-        comp.exec(6)
+@pytest.mark.parametrize(
+    'out1, out2, level, label, error',
+    [
+        (__XY_LABELS_1, __XY_LABELS_1, -1, False, ValueError),
+        (__XY_LABELS_1, __XY_LABELS_2, 6, True, ValueError),
+    ]
+)
+def test_errors(out1, out2, level, label, error):
+    with pytest.raises(error):
+        comparison_exec(out1, out2, level=level, label=label)
