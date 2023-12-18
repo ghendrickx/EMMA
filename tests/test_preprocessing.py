e46bbfe3fc6385cf335e0620a0caeb8b0895d74c
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-18 08:07:49 +0100

    REF: test_preprocessing.py > TestsPointsInFeature: refactored test-class to collection of test functions.

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -116,33 +116,33 @@ class TestProcessTimeSeries:
             assert pytest.approx(t) == o
 
 
-class TestPointsInFeature:
-    """Tests for `points_in_feature()`."""
+# Tests for `points_in_feature()`.
 
-    @pytest.mark.parametrize(
-        'points, length',
-        [
-            ([Point(2, 2)], 1),
-            ([Point(7, 7)], 0),
-            ([Point(1, 1), Point(2, 2), Point(3, 3)], 3),
-            ([Point(2, 2), Point(7, 7)], 1)
-        ]
-    )
-    def test_point_in_feature(self, dummy_feature, points, length):
-        out = pre.points_in_feature(dummy_feature, points)
-        assert len(out) == length
 
-    def test_assign_zes_code(self, dummy_feature):
-        out = pre.points_in_feature(dummy_feature, [Point(1, 1), Point(2, 2), Point(3, 3)])
-        assert all(v == 'Z2.222f' for v in out.values())
+@pytest.mark.parametrize(
+    'points, length',
+    [
+        ([Point(2, 2)], 1),
+        ([Point(7, 7)], 0),
+        ([Point(1, 1), Point(2, 2), Point(3, 3)], 3),
+        ([Point(2, 2), Point(7, 7)], 1)
+    ]
+)
+def test_point_in_feature(dummy_feature, points, length):
+    out = pre.points_in_feature(dummy_feature, points)
+    assert len(out) == length
 
-    @pytest.mark.parametrize(
-        'point, length',
-        [
-            ([Point(2, 2)], 1),
-            ([Point(7, 7)], 0)
-        ]
-    )
-    def test_quick_check(self, dummy_feature, point, length):
-        out = pre.points_in_feature(dummy_feature, point, quick_check=True)
-        assert len(out) == length
+def test_assign_zes_code(dummy_feature):
+    out = pre.points_in_feature(dummy_feature, [Point(1, 1), Point(2, 2), Point(3, 3)])
+    assert all(v == 'Z2.222f' for v in out.values())
+
+@pytest.mark.parametrize(
+    'point, length',
+    [
+        ([Point(2, 2)], 1),
+        ([Point(7, 7)], 0)
+    ]
+)
+def test_quick_check(dummy_feature, point, length):
+    out = pre.points_in_feature(dummy_feature, point, quick_check=True)
+    assert len(out) == length
