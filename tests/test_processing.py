aa708befff651a8b65bfcdbccc4f009b3a4ced1c
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-18 15:06:38 +0100

    REF: test_processing.py > test_map_ecotopes(): corrected function-name.

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -72,7 +72,7 @@ def test_determine_ecotopes():
         npt.assert_array_equal(x, np.array(y, dtype=(str if i == 2 else float)))
 
 
-def test_map_ecotope():
+def test_map_ecotopes():
     wd = __file__.split(os.sep)[:-2] + ['examples', 'ex_map_data']
     nc_file = os.sep.join(wd + ['output_map.nc'])
     csv_file = os.sep.join(wd + ['output.csv'])
