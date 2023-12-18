d631855507afc3ecaf45ada8b63c9016d78c70ec
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-12-18 10:38:49 +0100

    ADD: config_file.py > _update_nested_dict(): added function to update a nested dictionary.

--- config/config_file.py
+++ config/config_file.py
@@ -57,10 +57,45 @@ def load_config(f_default: str, f_user: typing.Union[str, dict] = None, wd: str 
         else:
             _LOG.info(f'Custom configuration file used: {file}')
 
+    elif isinstance(f_user, dict):
+        user = f_user.copy()
+
+    else:
+        msg = f'User-defined configuration must be either a `str` (referring to a JSON-file), ' \
+            f'or a `dict` matching the configuration-keywords; {type(f_user)} given'
+        raise TypeError(msg)
+
     # merge configurations
-    default.update(user)
+    default = _update_nested_dict(default, user)
     if user:
         _LOG.warning(f'Default configuration replaced: {user}')
 
     # return configuration
     return default
+
+
+def _update_nested_dict(d1: dict, d2: dict) -> dict:
+    """Update nested dictionary without removing non-updated keys/values.
+
+    :param d1: dictionary to be updated
+    :param d2: (partial) dictionary with updated keys/values
+
+    :type d1: dict
+    :type d2: dict
+
+    :return: updated, complete dictionary
+    :rtype: dict
+    """
+    # copy dictionaries
+    d1 = d1.copy()
+    d2 = d2.copy()
+
+    # update dictionary
+    for k, v in d2.items():
+        if isinstance(v, dict):
+            d1[k] = _update_nested_dict(d1.get(k, {}), v)
+        else:
+            d1[k] = v
+
+    # return updated dictionary
+    return d1
